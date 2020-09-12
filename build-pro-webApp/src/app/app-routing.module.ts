722c6dd438d20d7132119508eb4c371d1fccf79c
Aslene OUAZE <aslenerazor@gmail.com>
2020-09-12 18:54:40 +0100

    added tailwind css

--- build-pro-webApp/src/app/app-routing.module.ts
+++ build-pro-webApp/src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './shared/user/signin/front-singin.component';
 import { HomeComponent } from './components/home/HomeComponent';
 const routes: Routes = [
-  {path:'',component:HomeComponent},
-  {path:'signin',component:SigninComponent}
+  {path:'home',component:HomeComponent},
+  {path:'signin',component:SigninComponent},
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
